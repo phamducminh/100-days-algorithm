d7b7c1791ef7ba6a0e59ac982e7770c5ccca425d
Minh Pham <pdminh0410@gmail.com>
2018-12-07 00:51:10 +0700

    Day04: Two Sum - solution 2 - run 7 times faster

--- Day04/two-sum/two-sum.java
+++ Day04/two-sum/two-sum.java
@@ -16,3 +16,27 @@ class Solution {
         return result;
     }
 }
+
+// Runtime: 7 ms, faster than 59.74% of Java online submissions for Two Sum.
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff) && map.get(diff) != i) {
+                result[0] = i;
+                result[1] = map.get(diff);
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
