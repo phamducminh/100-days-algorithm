34b7b25959fdfe2033c6a7af5a34c0cadfecd3f3
Minh Pham <pdminh0410@gmail.com>
2018-12-09 00:28:20 +0700

    Day06: Single Number

---
+++ Day06/single-number/single-number.java
@@ -0,0 +1,30 @@
+// Runtime: 11 ms, faster than 25.53% of Java online submissions for Single Number.
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+            } else {
+                int value = map.get(nums[i]);
+                map.put(nums[i], ++value);
+            }
+        }
+
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            int key = ((Integer) pair.getKey()).intValue();
+            int value = ((Integer) pair.getValue()).intValue();
+
+            if (value == 1) {
+                result = key;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
