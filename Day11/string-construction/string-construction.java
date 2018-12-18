576a1a207158436a2027de2d95fd74df4c25a8ae
Minh Pham <pdminh0410@gmail.com>
2018-12-18 22:24:22 +0700

    Day11: String Construction

---
+++ Day11/string-construction/string-construction.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the stringConstruction function below.
+    static int stringConstruction(String s) {
+        int result = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (!set.contains(c)) {
+                result++;
+            }
+            set.add(c);
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = stringConstruction(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
