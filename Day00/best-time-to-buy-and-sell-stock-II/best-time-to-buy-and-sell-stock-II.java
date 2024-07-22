f2763e06094e8c0160b56b909d78ea1e3dc3dd10
Minh Pham <pdminh0410@gmail.com>
2024-07-22 08:23:37 -0700

    Day 00: Best Time to Buy and Sell Stock II

---
+++ Day00/best-time-to-buy-and-sell-stock-II/best-time-to-buy-and-sell-stock-II.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = 0;
+        
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        
+        return profit;
+    }
+}
