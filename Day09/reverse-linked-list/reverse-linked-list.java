533ec0d3c3e7d048c9990410a190e5f29b745bbb
Minh Pham <pdminh0410@gmail.com>
2024-07-30 14:47:43 -0700

    Day 09: Reverse Linked List

---
+++ Day09/reverse-linked-list/reverse-linked-list.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            
+            curr.next = prev;
+            
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
