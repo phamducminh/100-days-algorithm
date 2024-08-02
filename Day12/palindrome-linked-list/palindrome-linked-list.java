048a11ab2faa8a25982f02f1d5abfbc6a5a4df59
Minh Pham <pdminh0410@gmail.com>
2024-08-02 16:05:58 -0700

    Day 12: Palindrome Linked List

---
+++ Day12/palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            if (list.get(i) != list.get(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+                
+        return true;
+    }
+}
