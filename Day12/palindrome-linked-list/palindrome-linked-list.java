2ae560d2e24a917c5fc0ed6885e839bb3ad61954
Minh Pham <pdminh0410@gmail.com>
2024-08-02 16:22:49 -0700

    Day 12: Palindrome Linked List - O(n) time and O(1) space

--- Day12/palindrome-linked-list/palindrome-linked-list.java
+++ Day12/palindrome-linked-list/palindrome-linked-list.java
@@ -28,4 +28,36 @@ class Solution {
                 
         return true;
     }
+
+    public boolean isPalindrome2(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        // find middle (slow pointer)
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        // reverse second half
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        
+        // check palindrome
+        ListNode left = head;
+        ListNode right = prev;
+        while (right != null) {
+            if (left.val != right.val)
+                return false;
+            left = left.next;
+            right = right.next;
+        }
+        
+        return true;
+    }
 }
