95db90ee93041321ab8b8c0aacaeca55f6d833af
Minh Pham <pdminh0410@gmail.com>
2024-08-03 18:00:17 -0700

    Day 13: Linked List Cycle

---
+++ Day13/linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/773/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> hashSet = new HashSet<>();
+        while (head != null) {
+            if (hashSet.contains(head)) {
+                return true;
+            } else {
+                hashSet.add(head);    
+            }
+            head = head.next;
+        }
+        
+        return false;
+    }
+}
