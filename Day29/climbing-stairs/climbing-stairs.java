b80058dc5b3b77d1a0f6d78730aa627e3d042efb
Minh Pham <pdminh0410@gmail.com>
2024-08-19 20:37:19 -0700

    Day 29: Climbing Stairs

---
+++ Day29/climbing-stairs/climbing-stairs.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/569/
+class Solution {
+    public int climbStairs(int n) {
+        int one = 1, two = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        
+        return one;
+    }
+}
