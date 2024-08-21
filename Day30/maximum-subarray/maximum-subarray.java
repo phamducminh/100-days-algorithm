3fb5dc50dcab4778ed6dd051b558be8a8ddc075b
Minh Pham <pdminh0410@gmail.com>
2024-08-20 20:33:36 -0700

    Day 30: Maximum Subarray

---
+++ Day30/maximum-subarray/maximum-subarray.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/566/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSub = nums[0];
+        int curSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (curSum < 0)
+                curSum = 0;
+            
+            curSum += nums[i];
+            maxSub = Math.max(maxSub, curSum);
+        }
+        
+        return maxSub;
+    }
+}
