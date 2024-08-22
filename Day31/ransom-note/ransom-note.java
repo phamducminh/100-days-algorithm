aa55df2b2f8827711f4506b07533430eec89b56c
Minh Pham <pdminh0410@gmail.com>
2024-08-21 23:14:30 -0700

    Day 31: Random Note

---
+++ Day31/ransom-note/ransom-note.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/ransom-note/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < magazine.length(); ++i) {
+            ++cnt[magazine.charAt(i) - 'a'];
+        }
+        for (int i = 0; i < ransomNote.length(); ++i) {
+            if (--cnt[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
